cf48c4d18071c63b75c21b58135b517bb7da1454
brian.ujiie <bujiie+github@gmail.com>
2021-01-23 17:35:15 -0800

    Add new 900 level problems and update bootstrap to strip leading and trailing white spaces from output.

--- 900/Gravity_Flip_A/main.py
+++ 900/Gravity_Flip_A/main.py
@@ -3,6 +3,13 @@
 import fileinput
 
 with fileinput.input() as fp:
-	fp.readline().strip()
+	n = int(fp.readline().strip())
+	ai = list(map(lambda n: int(n), fp.readline().strip().split(' ')))
+
+	ai.sort()
+	c = list(map(lambda i: str(i), ai))
+	print(' '.join(c))
+		
+		
 
 		
